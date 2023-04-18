8393bd23bb847a6a38bbed2f1613ad7c5fd6f347
codewithdani <daneximpex@gmail.com>
2023-04-18 19:03:41 +0300

    for command chaining

--- shell.h
+++ shell.h
@@ -18,5 +18,10 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
 
 #endif /* SHELL_H */
